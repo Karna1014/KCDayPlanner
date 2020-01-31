2680be01a0518f1bc69125a72663b275c0004427
Java K <karna.cutolo@gmail.com>
2020-01-31 12:12:40 -0500

    Initial Readme and Final script update

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # KCDayPlanner
+## Single Day - Day Planner
+
+**Description:**
+
+A simple calendar application that allows the user to save events for each hour of the day.  App will run in the browser and features dynamically updated HTML and CSS powered by jQuery.  The following conditions were met:
+
+1. Planner is set for standard business hours (9 a.m. to 5 p.m.).
+2. Each time slot represents 1 hour block.
+3. Each block should contain the following:
+        a. The Hour
+        b. An Input Field
+        c. A Save/Commit Button
+4. "Save/Commit" will store time and user input in localStorage.
+5. Current Day/Date will update dynamically daily at top of page.
+6. Input Field is highlighted to denote current vs past and future events.
+
+
+**Usage:**
+Companies often proactively encourage the use of planners as a tool for time management.  Poor time management can be very costly resulting in missed meeting, deadlines or creating the appearance of unproffessionalism.  A planner allows employees to see their day at a glance, schedule time effectively, and improve productively.
+
+
+### File List:
+
+1. Index.html
+    
+    Main Page - Day Planner
+
+2. Quiz.js    
+    
+    Javascript for Main Page 
+
+3. Moment.js
+
+    Moment code
+
+4. Style.css
+
+    Stylesheet for all.js 
+
+5. Reset.css         
+
+    Compressed set of CSS rules that resets the styling of all HTML elements to a consistent baseline.
+
+
+### Link to site
+
+https://karna1014.github.io/KCDayPlanner/.
+
+
+### Index Page - Full Screen/Laptop View
+
+![Index Page](./Assets/Images/FullScreen-LaptopView.png)
+
+
+### Responsiveness - I-Pad (768px)
+
+![I-Pad](./Assets/Images/I-Pad.png)
+
+
+### Responsiveness - I-Phone 6-7-8 (375px)
+
+![I-Phone](./Assets/Images/I-Phone6-7-8.png)
+
--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var myInput5El = document.getElementById("myInput5");
 var myInput6El = document.getElementById("myInput6");
 var myInput7El = document.getElementById("myInput7");
 var myInput8El = document.getElementById("myInput8");
+var myInput9El = document.getElementById("myInput9");
 var save1El = document.getElementById("save1");
 var save2El = document.getElementById("save2");
 var save3El = document.getElementById("save3");
@@ -15,16 +16,165 @@ var save5El = document.getElementById("save5");
 var save6El = document.getElementById("save6");
 var save7El = document.getElementById("save7");
 var save8El = document.getElementById("save8");
+var save9El = document.getElementById("save9");
 
-currentDayEl = moment().format('dddd, MMMM Do YYYY');
-console.log(currentDayEl);
 
-save1El.addEventListener('click', function() {
+//posts 'today's date' at top of page
+var messageText = document.getElementById('currentDay');
+currentDayEl = moment().format('dddd, MMMM Do, YYYY');
+ messageText.innerHTML = (currentDayEl);
+
+$("#save1").on("click", function() {
+    var toDoEvent = {
+        "messageText" : myInput1El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("9:00a", JSON.stringify(toDoEvent));
     // upon clicking save button, all info in input line must
-        //save to local storage along with time stamp
-        //commit to input line allowing info to persist if page is refreshed
+      
+    //save to local storage along with time stamp
+    //commit to input line allowing info to persist if page is refreshed
         });
-currentTime = moment().format('LLL');
-//if(currentTime <= badge time highlight input as current event)
-    //else if ( highlight all others as future events) 
+        if(localStorage.getItem("9:00a") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("9:00a"));
+        myInput1El.value = toDoEvent["messageText"];
+    }
+
+
+
+$("#save2").on('click', function() {
+var toDoEvent = {
+    "messageText" : myInput2El.value,
+    "timeSave" : moment().format('hh:mm a')
+    };
+    console.log(toDoEvent);
+    localStorage.setItem("10:00a", JSON.stringify(toDoEvent));
+    });
+
+    if(localStorage.getItem("10:00a") != null) {
+    var toDoEvent = JSON.parse(localStorage.getItem("10:00a"));
+    myInput2El.value = toDoEvent["messageText"];
+    }
+
+$("#save3").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput3El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("11:00a", JSON.stringify(toDoEvent));
+        });
+        if(localStorage.getItem("11:00a") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("11:00a"));
+        myInput3El.value = toDoEvent["messageText"];
+        }
+
+$("#save4").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput4El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("12:00", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("12:00") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("12:00"));
+        myInput4El.value = toDoEvent["messageText"];
+        }
+$("#save5").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput5El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("1:00p", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("1:00p") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("1:00p"));
+        myInput5El.value = toDoEvent["messageText"];
+        }
+
+$("#save6").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput6El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("2:00p", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("2:00p") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("2:00p"));
+        myInput6El.value = toDoEvent["messageText"];
+        }
+
+$("#save7").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput7El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("3:00p", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("3:00p") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("3:00p"));
+        myInput7El.value = toDoEvent["messageText"];
+        }
+
+$("#save8").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput8El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("4:00p", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("4:00p") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("4:00p"));
+        myInput8El.value = toDoEvent["messageText"];
+        }
+
+$("#save9").on('click', function() {
+    var toDoEvent = {
+        "messageText" : myInput9El.value,
+        "timeSave" : moment().format('hh:mm a')
+        };
+        console.log(toDoEvent);
+        localStorage.setItem("5:00p", JSON.stringify(toDoEvent));
+        });
+
+        if(localStorage.getItem("5:00p") != null) {
+        var toDoEvent = JSON.parse(localStorage.getItem("5:00p"));
+        myInput9El.value = toDoEvent["messageText"];
+        }
+
+
+    //if (currentTime < time slot highlight all as future events)
+    //else if  (currentTime = time slot highlight input as current event)
     //else(highlight input as past event) 
+    // var currentHour = parseInt('19');  for testing purposes
+
+            var currentHour = parseInt(moment().format('HH'));
+            var hoursInDay = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+            
+            console.log(currentHour);                
+            hoursInDay.forEach(function(hour) {
+                var hourBadge = hour-8;
+                if (currentHour < hour) {
+                    document.getElementById("myInput" + hourBadge.toString()).style.backgroundColor = "mediumaquamarine";
+                    
+                } else if (currentHour === hour) {
+                    document.getElementById("myInput" + hourBadge.toString()).style.backgroundColor = "rgb(226, 238, 56";
+
+                } else {
+                    document.getElementById("myInput" + hourBadge.toString()).style.backgroundColor = "#a7490681";
+
+                }
+            })           
+            
+                                                                
